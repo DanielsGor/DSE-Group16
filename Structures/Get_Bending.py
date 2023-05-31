389b5664eac1ffe1a2c234a260fea72f89b6083d
Johnfet <99792694+Johnfet@users.noreply.github.com>
2023-05-31 14:28:26 +0200

    Bending calculations update

--- Structures/Get_Bending.py
+++ Structures/Get_Bending.py
@@ -1,25 +1,51 @@
 import numpy as np
-
+# from AirfoilData.scale_airfoil import scale_coordinates
 
 #   Dummy inputs
-Mx = np.arange(10, 0, -1) # Dummy bending distribution
+Mx = np.arange(10, 7, -1) # Dummy bending distribution
 
-My = np.arange(10, 0, -1)
+My = np.arange(10, 7, -1)
 
-Ixx = np.arange(10, 5, -0.5)
+Ixx = np.arange(10, 8.5, -0.5)
 
-Iyy = np.arange(10, 5, -0.5)
+Iyy = np.arange(10, 8.5, -0.5)
 
-Ixy = np.zeros(10)
+Ixy = np.zeros(3)
 
-#   Dummy coordinates for the maximum normal stress point
+#   Dummy wing coordinates
 
-xstress = 1.
+xwing = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
 
-ystress = 2.
+ywing = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+
+#   Neutral axis angle from cg coordinate frame
+
+tana = np.tan(-(My * Ixx - Mx * Ixy) / (Mx * Iyy - My * Ixy))
+
+#   Find maximum distance from NA for each span increment
+
+max_stress = np.zeros(len(My))
+max_stress_loc = np.zeros((2, len(My)))
+
+for i in range(len(My)):
+    na_dist = np.abs(tana[i] * xwing[i] - ywing[i]) / np.sqrt(tana[i] ** 2 + 1)
+
+    max_dist = max(na_dist)
+
+    # max_dist_i = na_dist.index(max_dist)
+
+    max_dist_i = np.argmax(na_dist)
+
+    max_stress[i] = ((Mx[i] * Iyy[i] - My[i] * Ixy[i]) * ywing[i][max_dist_i] + (My[i] * Ixx[i] - Mx[i] * Ixy[i]) * xwing[i][max_dist_i])\
+         / (Ixx[i] * Iyy[i] - Ixy[i] ** 2)
+
+    max_stress_loc[0][i] = np.array(xwing[i][max_dist_i])
+    max_stress_loc[1][i] = np.array(ywing[i][max_dist_i])
 
 #   Stress calculation
 
-Sz = ((Mx * Iyy - My * Ixy) * ystress + (My * Ixx - Mx * Ixy) * xstress) / (Ixx * Iyy - Ixy ** 2)
 
-print(Sz)
+
+print(max_stress, max_stress_loc)
+
+#   Outputs: maximum normal stress for each span increment

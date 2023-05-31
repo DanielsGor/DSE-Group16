1eb13fe9030334a3101d939096357fe78b0176bb
Johnfet <99792694+Johnfet@users.noreply.github.com>
2023-05-31 15:45:50 +0200

    Bending stress function finalized

--- Structures/Get_Bending.py
+++ Structures/Get_Bending.py
@@ -1,5 +1,4 @@
 import numpy as np
-# from AirfoilData.scale_airfoil import scale_coordinates
 
 #   Dummy inputs
 Mx = np.arange(10, 7, -1) # Dummy bending distribution
@@ -18,34 +17,35 @@ xwing = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
 
 ywing = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
 
-#   Neutral axis angle from cg coordinate frame
+def BendingStress(Mx, My, Ixx, Iyy, Ixy, xwing, ywing):
+    #   Neutral axis angle from cg coordinate frame
+    tana = np.tan(-(My * Ixx - Mx * Ixy) / (Mx * Iyy - My * Ixy))
 
-tana = np.tan(-(My * Ixx - Mx * Ixy) / (Mx * Iyy - My * Ixy))
+    #   Initialize arrays
+    max_stress = np.zeros(len(My))
 
-#   Find maximum distance from NA for each span increment
+    max_stress_loc = np.zeros((2, len(My)))
 
-max_stress = np.zeros(len(My))
-max_stress_loc = np.zeros((2, len(My)))
+    for i in range(len(My)):
+        #   Find maximum distance from NA for each span increment
 
-for i in range(len(My)):
-    na_dist = np.abs(tana[i] * xwing[i] - ywing[i]) / np.sqrt(tana[i] ** 2 + 1)
+        na_dist = np.abs(tana[i] * xwing[i] - ywing[i]) / np.sqrt(tana[i] ** 2 + 1)
 
-    max_dist = max(na_dist)
+        max_dist_i = np.argmax(na_dist)
 
-    # max_dist_i = na_dist.index(max_dist)
+        #   Stress calculation
 
-    max_dist_i = np.argmax(na_dist)
+        max_stress[i] = ((Mx[i] * Iyy[i] - My[i] * Ixy[i]) * ywing[i][max_dist_i] + (My[i] * Ixx[i] - Mx[i] * Ixy[i]) *
+                         xwing[i][max_dist_i]) \
+                        / (Ixx[i] * Iyy[i] - Ixy[i] ** 2)
 
-    max_stress[i] = ((Mx[i] * Iyy[i] - My[i] * Ixy[i]) * ywing[i][max_dist_i] + (My[i] * Ixx[i] - Mx[i] * Ixy[i]) * xwing[i][max_dist_i])\
-         / (Ixx[i] * Iyy[i] - Ixy[i] ** 2)
+        #   Assign maximum stress location values
 
-    max_stress_loc[0][i] = np.array(xwing[i][max_dist_i])
-    max_stress_loc[1][i] = np.array(ywing[i][max_dist_i])
+        max_stress_loc[0][i] = np.array(xwing[i][max_dist_i])
+        max_stress_loc[1][i] = np.array(ywing[i][max_dist_i])
 
-#   Stress calculation
+    return(max_stress, max_stress_loc)
 
-
+max_stress, max_stress_loc = BendingStress(Mx, My, Ixx, Iyy, Ixy, xwing, ywing)
 
 print(max_stress, max_stress_loc)
-
-#   Outputs: maximum normal stress for each span increment

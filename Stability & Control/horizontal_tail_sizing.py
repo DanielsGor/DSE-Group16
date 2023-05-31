a5f383a3dad8310ce8f3ec68f739d0ee53138356
sstenger <S.J.Stenger@student.tudelft.nl>
2023-05-31 16:58:00 +0200

    Update horizontal_tail_sizing.py

--- Stability & Control/horizontal_tail_sizing.py
+++ Stability & Control/horizontal_tail_sizing.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 #functions
 
@@ -19,44 +19,71 @@ def calculate_C_L_Ah_alpha(C_L_w_alpha, lamda, b, b_fus, c_root, S):
 #calculate C_L_h_alpha
 def calculate_C_L_h_alpha(A_h, lamda_h, C_l_alpha_h):
     E = 1 + 2 * lamda_h / (A_h * (1 + lamda_h))
-    C_L_alpha_h = 0.995 * C_l_alpha_h / (E + C_l_alpha_h/(np.pi * A_h))
+    C_L_h_alpha = 0.995 * C_l_alpha_h / (E + C_l_alpha_h/(np.pi * A_h))
     return C_L_h_alpha
 
 #constants & inputs
 
+#environmental constants
+rho = 1.225 # [kg/m^3] air density
+g = 9.80665 # [m/s^2] gravitational acceleration
+T = 288.15 # [K] temperature
+
+#aircraft constants
+m = 7.68 # [kg] aircraft mass
+W = m * g # [N] aircraft weight
+
 #wing constants
 S = 1.1# [m^2] wing surface area
 b = 2.95# [m] wing span
-b_fus = # [m] fuselage width
-C_l_alpha = # [-] lift curve slope wing
-A = 7.91136# [-] aspect ratio wing
+b_fus = 0.25 # [m] fuselage width
+C_l_alpha = 6.161 # [-] lift curve slope wing
+A = 7.91136 # [-] aspect ratio wing
 lamda = 0.45 # [-] taper ratio wing
-c_root = 0.49078# [m] root chord wing
+c_root = 0.49078 # [m] root chord wing
 mac = 0.37288 # [m] mean aerodynamic chord wing
 VhV2 = 1 # [-] ratio of horizontal tail velocity to aircraft velocity
 
 #horizontal tail constants
-A_h = # [-] aspect ratio horizontal tail
-lamda_h = # [-] taper ratio horizontal tail
-C_l_alpha_h = # [-] lift curve slope horizontal tail
+A_h = 5 # [-] aspect ratio horizontal tail
+lamda_h =  0.8 # [-] taper ratio horizontal tail
+C_l_alpha_h = 0.1 * np.pi/180 # [-] lift curve slope horizontal tail
 
-
-x_bar_cg = 
-x_bar_ac =
+x_bar_cg = np.linspace(-1, 2, 1000)
+x_bar_ac = 0.25 # [-] aerodynamic center position #to be adapted once we have the data
 SM = 0.05 #stability margin
+
+#maneuvrability
+r = 50 # [m] turn radius
+ROC = 10 # [m/s] rate of climb #guessed out of thin air
+deltah = 80 # [m] change in altitude for climb
+climb_angle = np.arctan(deltah/(np.pi * r)) # [rad] climb angle
+v_climb = ROC /np.sin(climb_angle) # [m/s] climb speed
 
 C_L_h_alpha = calculate_C_L_h_alpha(A_h, lamda_h, C_l_alpha_h)
 C_L_w_alpha = calculate_C_L_w_alpha(A, lamda, C_l_alpha)
 C_L_Ah_alpha = calculate_C_L_Ah_alpha(C_L_w_alpha, lamda, b, b_fus, c_root, S)
-C_L_h = #tbd
-C_L_Ah = #tbd
-C_m_ac =  #to be changed
-deda = 0 #to be changed
+C_L_h = -0.35 * A_h**(1/3) # Lift coefficient of the horizontal tail
+C_L_Ah = 2 * W / (rho * v_climb**2 * S) # Lift coefficient of the main wing
+C_m_ac = -0.16 # [-]
+deda = 0 #depends on the tail configuration # formula E-52 in Torenbeek, r and m in fig E-13
 
 # Stability line
 
-htail_volume = (x_bar_cg - x_bar_ac + SM) / (C_L_alpha_h / C_L_alpha_Ah * (1 - deda) * VhV2)
+htail_volume_stability = (x_bar_cg - x_bar_ac + SM) / (C_L_h_alpha / C_L_Ah_alpha * (1 - deda) * VhV2)
 
 # Control line
 
-htail_volume = 1 / (C_L_h / C_L_Ah * VhV2) * x_bar_cg + (C_m_ac / C_L_Ah - x_bar_ac) / (C_L_h / C_L_Ah * VhV2)
+htail_volume_control = 1 / (C_L_h / C_L_Ah * VhV2) * x_bar_cg + (C_m_ac / C_L_Ah - x_bar_ac) / (C_L_h / C_L_Ah * VhV2)
+
+
+#print('Stability line: ', htail_volume_stability)
+#print('Control line: ', htail_volume_control)
+
+#plotting
+#plt.plot(x_bar_cg, htail_volume_stability, label = 'Stability line')
+plt.plot(x_bar_cg, htail_volume_control, label = 'Control line')
+plt.xlabel('x_cg_bar')
+plt.ylabel('Volume_h_bar')
+plt.legend()
+plt.show()

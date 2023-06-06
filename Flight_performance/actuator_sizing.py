5ba7d0f6bd8546e59c31db0ff2238014eb2244ae
tbejan <t.bejan@student.tudelft.nl>
2023-06-06 14:52:48 +0200

    Changed the velocity calculation method in Horizontal_tail_sizing

--- Flight_performance/actuator_sizing.py
+++ Flight_performance/actuator_sizing.py
@@ -4,15 +4,10 @@
     3. The flow is incompressible
     4. The validity of estimation methods has been previously established under Horizontal Tail Sizing
     5. Velocity is constant over the flight phase
-    6. Lift during the steady pull up manoeuver stays constant"""
-
+    6. Lift during the steady pull up maneuver stays constant"""
 
 import numpy as np
-
-from roll_rate import dt, Rs, m
-from Stability.Horizontal_tail_sizing_final import deda, VhV2
-from Stability.tail_sizing import htail_volume
-from pitch_rate import
+from pitch_rate import R_min, R_min_index, global_pitch_angles, pitch_dot2, times
 
 ####Constants####
 rho = 1.225
@@ -21,41 +16,33 @@ V = 15.1
 
 ####Plasma####
 delta_CL_plasma = 0.06
-
-
-
+delta_Cm_plasma = 0.1
+MAC_plasma = 0.11
 
 ####Aircraft####
-x_cg = 0.5
+x_cg = -0.7
 m = 7.68
-theta_dot2 = 0
-
-
 
 ####Wing####
 Cm_ac = -0.16
 Cm_ac_h = -0.01
 x_w_bar = 0.25
-MAC = 0.3
-CL_alpha = 0
+MAC = 0.37
+CL_alpha = 6.161
 S = 1.1
 
-n = 1.38
-
-
-
 ####Tail####
-CL_alpha_h = 0
-
+htail_volume = 0.359
+CL_alpha_h = 0.1*180/np.pi
 S_h = 0.1175
 A_h = 5
 MAC_h = np.sqrt(S_h/A_h)
+l_h = htail_volume * S * MAC / MAC_h
+print(l_h)
 
-x_np = MAC*(CL_alpha_h/CL_alpha)*(1-deda)*VhV2*htail_volume+x_w_bar
+theta_dot2 = pitch_dot2(global_pitch_angles, times, R_min_index)
+print(R_min)
+S_plasma = (m*R_min**2*theta_dot2)/(0.5*1.225*V**2*(delta_CL_plasma*l_h+delta_Cm_plasma*MAC_plasma))
 
-x_bar = x_cg - x_np
-
-S_p = (m*V**2/g0 / (n-1)-0.5*rho*(Cm_ac*S*MAC+VhV2*S_h*MAC_h*Cm_ac_h))/(delta_CL_plasma*x_bar)
-
-
-
+print("Thetadot2 is:", theta_dot2)
+print("The surface of the plasma actuator is:", S_plasma)

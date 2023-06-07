eb3e54210ba73c90b2bcb0b6e2ed0a60fd4998c2
alang321 <34992832+alang321@users.noreply.github.com>
2023-06-08 00:15:20 +0200

    somewhat not totally broken anymore

--- Landing System/airbag.py
+++ Landing System/airbag.py
@@ -6,20 +6,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 gamma = 1.4
-density_a = [1.28, 1.127, 1.225]  # kg/m^3
-pressure_a = [101262.784, 101288.0544, 101306.3363]  # Pa
+density_a = [1.28, 1.225, 1.127]  # kg/m^3
+pressure_a = [101262.784, 101325, 101306.3363]  # Pa
 temperature_a = [275.65, 288.15, 313.15]  # K
 R = 287  # J/kgK
 g = 9.80665  # m/s^2
 
 # DEFINE THESE CONSTANTS
-P_init = 1  # -, initial bag pressure ratio P_bag/P_a, depends on chosen fan, max back pressure
+p_bag_init = 101325  # -, initial bag pressure ratio P_bag/P_a, depends on chosen fan, max back pressure
+rho_bag_init = 1.225  # -, initial bag pressure ratio P_bag/P_a, depends on chosen fan, max back pressure
 initial_velocity = 4.202856172  # m/s, initial velocity of the uav
 B2 = 0.7 * 0.15  # m2, lower side bag footprint area
 B1 = B2  # m2, upper side bag footprint area
 #B1 = 0.8 * 0.4  # m2, upper side bag footprint area
-h0 = 0.4  # m, initial height of the bag
-a = 0.018  # m2, orifice area
+h0 = 20  # m, initial height of the bag
+A_or = 0.00000018  # m2, orifice area
 M = 7.68  # kg, mass of the UAV
 
 def B_avg(h):
@@ -28,51 +29,97 @@ def B_avg(h):
 def B(h):
     return (B2 - (B2 - B1)/h0 * h)
 
-def Cd(P):
-    return 0.9 - 0.3 / P
+h = h0
+dh_dt = initial_velocity
+p_a, T = pressure_a[1], temperature_a[1]
+p_bag = p_bag_init
 
-def rho_bag(rho_a, P_bag, P_a):
-    return rho_a * (P_bag / P_a) ** (1 / gamma)
+V_t = B2 * h
 
-def speed_of_sound(T):
-    return np.sqrt(gamma * R * T)
+m_t = p_bag * V_t / (R * T)
 
-def system(t, y):
-    # Define the derivatives
-    h, dh_dt, P = y
+time_steps = np.linspace(0, 15, 1000)  # seconds
 
-    d2h_dt2 = -g + (P - 1) * p_a * B(h) / M
+delta_t = 1 / len(time_steps)
 
+accel_list = []
+time_steppos = []
+m_list = []
+p_bag_list = []
+h_list = []
+vel_list = []
+bag_force_list = []
 
-    c = speed_of_sound(T)
-    dP_dt = (P**(1/gamma) * dh_dt * B(h) - a * Cd(P) * np.sqrt((2 * gamma)/(gamma - 1) * (c**2 / gamma) * (P**(1 - 1/gamma) - 1)))/(B_avg(h) * h/gamma * P ** (1/gamma - 1))
-
-    dy_dt = np.array([
-        dh_dt,
-        d2h_dt2,
-        dP_dt
-    ])
-    # Return the derivatives
-    return dy_dt
-
-y_0 = [h0, initial_velocity, P_init]
-
-p_a, T = pressure_a[0], temperature_a[0]
-
-# Solve the system using odeint
-solution = scipy.integrate.solve_ivp(system, [0, 1], y_0, method='RK45', max_step=0.01)
-
-print(solution)
+for t in time_steps:
+    rho_bag = m_t / V_t
+    p_bag = (rho_bag / rho_bag_init) ** (gamma - 1) ** (gamma / (gamma - 1)) * p_bag_init
 
 
-yy = solution.y[0]
-tt = solution.t
-print(yy)
-print(tt)
+    d2h_dt2 = g - (p_bag - p_a) * B(h) / M
+    h = h - dh_dt * delta_t - 0.5 * d2h_dt2 * delta_t ** 2
+    dh_dt = dh_dt + d2h_dt2 * delta_t
+
+    V_t = B2 * h
+
+    labda = p_a / p_bag
+
+    print(labda)
+
+    C_D = -3.8399 * labda ** 6 + 9.4363 * labda ** 5 - 7.2326 * labda ** 4 + 1.6972 * labda ** 3 - 0.2908 * labda ** 2 - 0.013 * labda + 0.8426
+
+    dm_dt = C_D * A_or * p_a * (1 / R / T) ** .5 * ((2 * gamma)/(gamma - 1) * (p_bag_init/p_a)**((gamma - 1)/(gamma))) ** .5 * ((p_bag/p_a)**((gamma - 1)/gamma) - 1) ** .5
+
+    m_t = m_t - dm_dt * delta_t
+
+
+    #stop if mt is complex
+    if np.iscomplex(m_t):
+        break
+
+    accel_list.append(d2h_dt2)
+    time_steppos.append(t)
+    m_list.append(m_t)
+    p_bag_list.append(p_bag)
+    h_list.append(h)
+    vel_list.append(dh_dt)
+    bag_force_list.append((p_bag - p_a) * B(h))
+
+
+    if m_t <= 0:
+        break
+
+
 
 
 # plot the h position
-plt.plot(tt, yy)
+#plt.plot(time_steppos, accel_list)
+#plt.plot(time_steppos, m_list)
+plt.plot(time_steppos, h_list)
+#plt.plot(time_steppos, p_bag_list)
 plt.xlabel('time')
 plt.ylabel('h(t)')
+plt.show()
+
+plt.plot(time_steppos, p_bag_list)
+plt.xlabel('time')
+plt.ylabel('pressure')
+plt.show()
+
+plt.plot(time_steppos, vel_list)
+plt.xlabel('time')
+plt.ylabel('velocity')
+plt.show()
+
+
+plt.plot(time_steppos, accel_list)
+plt.xlabel('time')
+plt.ylabel('accel')
+plt.show()
+
+
+plt.plot(time_steppos, bag_force_list, label="bag force")
+plt.plot(time_steppos, [M*g for i in range(len(time_steppos))], label="Gravity")
+plt.xlabel('time')
+plt.ylabel('Newton')
+plt.legend()
 plt.show()

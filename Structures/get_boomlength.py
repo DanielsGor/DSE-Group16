fea02d2c3af0841115b72a1ca0364b1c27631947
Jasper Tillie <jptillie@student.tudelft.nl>
2023-06-07 17:14:17 +0200

    first boom functions

--- Structures/get_boomlength.py
+++ Structures/get_boomlength.py
@@ -1,2 +1,11 @@
 import numpy as np
-
+import math
+def inertia_thin_walled_circular_section(thickness, diameter)
+    Ix = (np.pi * thickness * diameter ** 2) / 8
+    J = (np.pi * thickness * diameter ** 2) / 4
+    return(Ix, J)
+def get_mass(thickness, diameter, rho, length):
+    mass = np.pi*diameter*thickness*rho*length
+    return(mass)
+def deflection_angle_by_pointforce(force, length, E_modulus, Ix):
+    d

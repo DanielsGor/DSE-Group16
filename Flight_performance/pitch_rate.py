846f1bc923de23f19efd8369075efd532f52baf4
tbejan <t.bejan@student.tudelft.nl>
2023-06-07 15:51:21 +0200

    Updated and working version of pitch.py

--- Flight_performance/pitch_rate.py
+++ Flight_performance/pitch_rate.py
@@ -1,26 +1,29 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-### inputs ###
+#### Constants ####
+dt = 0.001 #s
+g = 9.81 #m/s2
+
+#### Aircraft ####
 LD_plane = 14 #inital guess TODO:to change
 glide_angle = np.arctan(1/LD_plane) #rad
 global_pitch_angle = glide_angle
 climb_angle = 7.85 #deg
-g = 9.81 #m/s2
+
 m = 7.68 #kg
-dt = 0.001 #s
+
 V = 15.1 #m/s
-cl_cruise = 0.92
+cl_cruise = 0.4987
 rho = 1.225 #kg/m3
 S = 1.1 #m2
 
 cD0 = 0.01571
 e = 0.7
 A = 7.911
-cd_cruise = cD0 + cl_cruise**2/(np.pi * A *e) 
+cd_cruise = cD0 + cl_cruise**2/(np.pi * A *e)
 
-
-T_climb = m*g*np.sin(np.deg2rad(-climb_angle)) + cd_cruise * S * 0.5 * V**2 * rho
+T_climb = m*g*np.sin(np.deg2rad(climb_angle)) + cd_cruise * S * 0.5 * V**2 * rho
 
 x = [0]
 y = [0]
@@ -31,12 +34,14 @@ pitch_rates = [0]
 iter = 0
 Rs = [0]
 accs  =[0]
+counter = 0
+
 
 while global_pitch_angle > np.deg2rad(-climb_angle):
     time = times[-1] + dt
     global_pitch_angle = global_pitch_angles[-1]
     V = Vs[-1]
-    a_tangential = (T_climb + m*g * np.sin(global_pitch_angle) - cd_cruise *0.5 * rho * V**2 * S)/m
+    a_tangential = (T_climb + m*g * np.sin(global_pitch_angle) - cd_cruise * 0.5 * rho * V**2 * S) / m
     a_centripetal = cl_cruise * 0.5 * rho * V**2 * S/m - g * np.cos(global_pitch_angle)
     R = V**2 / a_centripetal
     Rs.append(R)
@@ -55,16 +60,29 @@ while global_pitch_angle > np.deg2rad(-climb_angle):
     global_pitch_angles.append(global_pitch_angle)
     times.append(time)
     Vs.append(V)
+    counter += 1
+
+
+
+
 
 Ls = [cl_cruise * 1/2 * rho * Vcurrent ** 2 * S for Vcurrent in Vs]
-Ws = m * g * np.cos(global_pitch_angles)
+Ws = m * g / np.cos(global_pitch_angles)
 ns = Ls/Ws
-print(V)
 
-print (np.mean(pitch_rates), max(pitch_rates))
-# plt.plot(times, Rs)
-print (max(ns))
-plt.plot (times,ns)
-plt.show()
+# plt.plot(times, ns)
+# plt.show()
+
+
+# plt.plot(times, pitch_rates)
+# plt.show()
+
+# plt.plot (x, y)
+# plt.show()
+
+
+print("Vmax is:", max(Vs), "m/s")
+print("The limiting pitch rate is: ", max(pitch_rates)*180/np.pi, " deg/s")
+print("The limiting load factor is: ", max(ns))
 
 

1503f57b49aa6f110fca1e95ffa35ebb55c037a4
Louis Bruninx <l.f.l.bruninx@student.tudelft.nl>
2023-06-08 14:22:43 +0200

    updates wind tunnel test

--- Stability/wind_tunnel_test/test_matrix_creation.py
+++ Stability/wind_tunnel_test/test_matrix_creation.py
@@ -10,10 +10,10 @@ alpha_long = [0, 4, 8, 10, 12]
 V_pp_short = [0, 9, 12]
 V_pp_long = [0, 6, 9, 12]
 
-"""
+
 V_pp_short = [12]
 V_pp_long = [12]
-"""
+
 
 f_burst_short = [10, 50, 200]
 f_burst_long = [10, 20, 50, 100, 200, 500]
@@ -24,50 +24,56 @@ duty_cycle_long = [0.1, 0.3, 0.5, 0.7, 0.9]
 
 
 def test_1(alpha = alpha_long, V_pp=V_pp_short, f_burst=f_burst_short, duty_cycle=duty_cycle_short):
-    df_test_1 = pd.DataFrame(columns=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])
+    df_test_1 = pd.DataFrame(columns=['test series', 'alpha', 'V_pp', 'f_burst', 'duty_cycle'])
     for a  in alpha:
         for v in V_pp:
             for f in f_burst:
                 for d in duty_cycle:
-                    df_test_1 = pd.concat([df_test_1, pd.DataFrame([[a, v, f, d]], columns=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])])
+                    df_test_1 = pd.concat([df_test_1, pd.DataFrame([[1, a, v, f, d]], columns=['test series', 'alpha', 'V_pp', 'f_burst', 'duty_cycle'])])
 
     print('break')
     return df_test_1
 
 def test_2(alpha = alpha_short, V_pp=V_pp_long, f_burst=f_burst_short, duty_cycle=duty_cycle_short):
-    df_test_2 = pd.DataFrame(columns=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])
+    df_test_2 = pd.DataFrame(columns=['test series', 'alpha', 'V_pp', 'f_burst', 'duty_cycle'])
     for a in alpha:
         for v in V_pp:
             for f in f_burst:
                 for d in duty_cycle:
-                    df_test_2 = pd.concat([df_test_2, pd.DataFrame([[a, v, f, d]], columns=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])])
+                    df_test_2 = pd.concat([df_test_2, pd.DataFrame([[2, a, v, f, d]], columns=['test series', 'alpha', 'V_pp', 'f_burst', 'duty_cycle'])])
     print('break')  
     return df_test_2
 
 def test_3(alpha = alpha_short, V_pp=V_pp_short, f_burst=f_burst_long, duty_cycle=duty_cycle_short):
-    df_test_3 = pd.DataFrame(columns=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])
+    df_test_3 = pd.DataFrame(columns=['test series', 'alpha', 'V_pp', 'f_burst', 'duty_cycle'])
     for a in alpha:
         for v in V_pp:
             for f in f_burst:
                 for d in duty_cycle:
-                    df_test_3 = pd.concat([df_test_3, pd.DataFrame([[a, v, f, d]], columns=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])])
+                    df_test_3 = pd.concat([df_test_3, pd.DataFrame([[3, a, v, f, d]], columns=['test series', 'alpha', 'V_pp', 'f_burst', 'duty_cycle'])])
     print('break')   
     return df_test_3
 
 def test_4(alpha = alpha_short, V_pp=V_pp_short, f_burst=f_burst_short, duty_cycle=duty_cycle_long):
-    df_test_4 = pd.DataFrame(columns=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])
+    df_test_4 = pd.DataFrame(columns=['test series', 'alpha', 'V_pp', 'f_burst', 'duty_cycle'])
     for a in alpha:
         for v in V_pp:
             for f in f_burst:
                 for d in duty_cycle:
-                    df_test_4 = pd.concat([df_test_4, pd.DataFrame([[a, v, f, d]], columns=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])])
+                    df_test_4 = pd.concat([df_test_4, pd.DataFrame([[4, a, v, f, d]], columns=['test series', 'alpha', 'V_pp', 'f_burst', 'duty_cycle'])])
     print('break')
     return df_test_4
 
 def combined_test(test_1=test_1(), test_2=test_2(), test_3=test_3(), test_4=test_4()):
     df_combined = pd.concat([test_1, test_2, test_3, test_4])
     # Remove duplicates from df_combined
-    df_combined = df_combined.drop_duplicates()
+    df_combined = df_combined.drop_duplicates(subset=['alpha', 'V_pp', 'f_burst', 'duty_cycle'])
+    # get rid of duplicates when V_pp = 0
+
+    '''
+    Input code to remove duplicates when V_pp = 0
+    '''
+
     df_combined = df_combined.reset_index(drop=True)
 
     # Given that every test takes 3 minutes, how long will it take to run all tests?

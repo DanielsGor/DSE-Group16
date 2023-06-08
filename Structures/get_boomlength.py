c899b08a6b9a7c09bbf4cd56d57f143c2b847d3d
Johnfet <99792694+Johnfet@users.noreply.github.com>
2023-06-08 11:38:41 +0200

    Added bending stress function for the tail booms

--- Structures/get_boomlength.py
+++ Structures/get_boomlength.py
@@ -1,20 +1,26 @@
 import numpy as np
 import math
 
-
-
 #   Calculate maximum bending stress for different dimensions
-def BendingStress(Mx, My, Ixx, Iyy, Ixy, xboom, yboom):
+#   Assume Ixy for tail booms is 0
+def BendingStress(Mx, My, Ixx, Iyy, type, width, height):
     #   Neutral axis angle from cg coordinate frame
-    tana = np.tan(-(My * Ixx - Mx * Ixy) / (Mx * Iyy - My * Ixy))
+    tana = np.tan(-(My * Ixx) / (Mx * Iyy))
+
+    if type == 'circular':
+        xloc1 = width * np.cos(np.arctan(tana) + np.pi / 2)
+        xloc2 = width * np.cos(np.arctan(tana) - np.pi / 2)
+
+
+
 
     #   Find maximum distance from NA for each span increment
     na_dist = np.abs(tana * xboom - yboom) / np.sqrt(tana ** 2 + 1)
     max_dist_i = np.argmax(na_dist)
 
     #   Stress calculation
-    max_stress = ((Mx * Iyy - My * Ixy) * yboom[max_dist_i] + (My * Ixx - Mx * Ixy) *
-                     xboom[max_dist_i]) / (Ixx * Iyy - Ixy ** 2)
+    max_stress = ((Mx * Iyy) * yboom[max_dist_i] + (My * Ixx) *
+                     xboom[max_dist_i]) / (Ixx * Iyy)
 
     #   Assign maximum stress location values
     max_stress_loc = [np.array(xboom[max_dist_i]), np.array(yboom[max_dist_i])]
@@ -28,12 +34,19 @@ def inertia_thin_walled_circular_section(thickness, diameter)
     J = (np.pi * thickness * diameter ** 2) / 4
     return(Ix, J)
 
-def circular_boom_weight(type, thicknessV, thicknessH, rho, length, width, height):
+def boom_properties(type, thicknessV, thicknessH, rho, length, width, height):
+    Ixx = None
+    Iyy = None
+    mass = None
     if type == 'circular':
         mass = np.pi*width*thicknessV*rho*length
+        Ixx = (np.pi * thicknessV * width ** 2) / 8
+        Iyy = Ixx
     elif type == 'rectangular':
         mass = (2*width*thicknessH+2*height*thicknessV)*length*rho
-    return(mass)
+        Ixx = 2 * ((height*thicknessV**3)/12 + width*thicknessV*(height/2)**2)
+        Iyy = 2 * ((width*thicknessH**3)/12 + height*thicknessH*(width/2)**2)
+    return(mass, Ixx, Iyy)
 
 
 def deflection_angle_by_pointforce(force, length, E_modulus, Ix):
